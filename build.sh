60f1757c33f75dc34917bb92aadecf1b58cc4aa6
magnetrwn <magnetrwn@tuta.io>
2026-06-21 11:54:16 +0200

    Build using only git submodules for both raylib and inipp.

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -e
 mkdir -p build
 rsync -a static/ build/
 
+cd lib/raylib
+mkdir -p build && cd build
+cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DGRAPHICS=GRAPHICS_API_OPENGL_43
+cmake --build . -j$(nproc) --config Release
+cd ../../..
+
 cd build
 cmake -G "Unix Makefiles" ..
 make -j$(nproc)
